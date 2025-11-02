43b48a0a99d8a9eb06e5cc4fe49a5154d1d4f14f
Thomas Brockmans <thomas.brockmans@student.vives.be>
2025-11-02 13:33:42 +0100

    added first part of export class

--- models/export.py
+++ models/export.py
@@ -8,3 +8,33 @@ Created on Sun Nov  2 13:12:17 2025
 @author: thomasbrockmans
 """
 
+from ride import Ride
+from route import Route
+import csv
+from datetime import datetime
+
+class Export:
+    
+    def __init__(self):
+        pass
+    
+    
+    # classmethod to export all routes to csv
+    @classmethod
+    def export_all_routes(cls):
+        list_routes = Route.get_all()
+        
+        
+        now = datetime.now()
+        now_string = now.strftime("%Y%m%d%H%M%S")
+        file_name = "export_routes_" + now_string + ".csv"
+        print(file_name)
+        
+        with open(file_name, 'w',newline='') as file:
+            writer = csv.writer(file, delimiter=';')
+        
+            for route in list_routes:
+                pass
+            
+if __name__ == '__main__':
+    Export.export_all_routes()
